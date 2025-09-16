daa2c70cef86119a0b2abb4b44e5050496f43782
Ethanscharlie <ethanscharlie@protonmail.com>
2025-09-16 00:23:36 -0600

    More

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ peer.on('open', function(id) {
 	console.log('My peer ID is: ' + id);
 });
 
+
+
+peer.on('connection', function(conn) { 
+  console.log("Got connection form")
+  console.log(conn)
+});
+
+var conn
 function inputID() {
   var input = prompt("What is id?");
   var conn = peer.connect(input);
