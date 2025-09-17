e67ae13a70c2a42148e45b7c11a94477dc4e749d
Ethanscharlie <ethanscharlie@protonmail.com>
2025-09-16 22:25:11 -0600

    Can finally fucking send mesasges back and forth

---
+++ main.js
@@ -0,0 +1,37 @@
+class Server {
+  constructor(callback) {
+    this.peer = new Peer();
+    this.connections = [];
+    this.messages = "";
+    this.id;
+    this.callback = callback;
+
+    this.peer.on("open", (id) => {
+      this.id = id;
+      console.log(id);
+
+      document.getElementById("servercode").innerText = id;
+
+      this.callback(id);
+    });
+
+    this.peer.on("connection", (conn) => {
+      console.log("Got connection from " + conn.peer);
+      this.connections.push(conn);
+    
+      conn.on("data", (data) => {
+        this.messages += data + "\n";
+        console.log(data);
+    
+        this.updateForAllClients();
+      });
+    });
+  }
+
+  updateForAllClients() {
+    this.connections.forEach((conn) => {
+      conn.send(this.messages);
+    });
+  }
+}
+
