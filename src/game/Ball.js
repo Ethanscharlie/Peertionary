4a57e434a7988a433b039fe021a7f9e465a2728a
Ethanscharlie <ethanscharlie@protonmail.com>
2025-09-18 22:16:41 -0600

    Ball that I can throw around

--- src/game/Ball.js
+++ src/game/Ball.js
@@ -1,17 +1,47 @@
 class Ball {
   constructor(canvas) {
     this.RADIUS = 15;
+    this.DRAG = 0.1;
 
     this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
+
     this.x = 0;
     this.y = 0;
+
+    this.velocity = {'x': 0, 'y': 0};
   }
 
   setPosition(x, y) {
     this.x = x;
     this.y = y;
     return this;
+  }
+
+  updatePhysics() {
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+
+    this.velocity.x = this.applyDragToNumber(this.velocity.x);
+    this.velocity.y = this.applyDragToNumber(this.velocity.y);
+  }
+
+  isMoving() {
+    return this.velocity.x != 0 && this.velocity.y != 0;
+  }
+
+  applyDragToNumber(number) {
+    if (number > this.DRAG) {
+      number -= this.DRAG;  
+      return number;
+    }
+  
+    if (number < -this.DRAG) {
+      number += this.DRAG;  
+      return number;
+    }
+  
+    return 0;
   }
 
   draw() {
